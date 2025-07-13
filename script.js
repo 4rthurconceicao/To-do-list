0fe81dadbbd7a374ab5c1bc30e9981dc0fa31a18
4rthurconceicao <arthurconceicaosouza1@gmail.com>
2025-07-13 18:11:32 -0300

    àdd

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+Lista de tarefas
+[x]Saber quando o botão foi clicado
+[x]Pegar o texta dentro do Input
+[]Colocar esse texto na tela
+[]
+ */
+
+function adicionarTarefa(){
+let tarefa = document.getElementById('nova-tarefa').value
+
+
+let lista = document.createElement('li')
+li.innerHTML = tarefa +`<button>Delete</button>`
+console.log(li)
+}
+
